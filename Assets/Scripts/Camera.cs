9a0c2e74130f21ab0a460aac1281d8cd420f6995
ABC <keso259w@gmail.com>
2018-08-13 09:06:58 +0900

    スクリプトファイル　を　作りました

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 LookPosi;
+    public Vector3 distance;
+
+    private void Update()
+    {
+        this.transform.position = target.transform.position - distance;
+        this.transform.LookAt(target.transform.position + LookPosi);
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public GameObject camera;
+
+    float x;
+    float z;
+
+    Vector3 angle;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+        angle = transform.eulerAngles;
+
+        if(Input.GetKey("up"))
+        {
+            this.transform.eulerAngles = new Vector3(angle.x, (45 * x) + camera.transform.localRotation.y, angle.z);
+        }
+        else if (Input.GetKey("down"))
+        {
+            this.transform.eulerAngles = new Vector3(angle.x, (180 * z - 45 * x) + camera.transform.localRotation.y, angle.z);
+        }
+        else if(Input.GetKey("right") || Input.GetKey("left"))
+        {
+            this.transform.eulerAngles = new Vector3(angle.x, (90 * x) + camera.transform.localRotation.y, angle.z);
+        }
+        
+       
+    }
+}
