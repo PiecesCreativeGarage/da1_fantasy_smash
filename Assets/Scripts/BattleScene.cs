3788d913986552821dc33c540c56891d4da97bcf
川口薫 <>
2018-08-15 20:34:39 +0900

    敵を場に生成

---
+++ Assets/Scripts/BattleScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Object prefab = Resources.Load ("Character/2HandedWarrior");
+		GameObject go = (GameObject)Instantiate (prefab);
+		go.transform.localEulerAngles = new Vector3 (0, 180, 0);
+		go.transform.position = new Vector3 (10, 0, 10);
+	}
+}
