e078037cc235ff4b5f86ed4d351c473cb6273626
川口薫 <>
2018-08-15 22:03:41 +0900

    Stageモデルを追加

--- Assets/Scripts/BattleScene.cs
+++ Assets/Scripts/BattleScene.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BattleScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		SceneManager.LoadScene ("BgStage1", LoadSceneMode.Additive); 
+
 		Object prefab = Resources.Load ("Character/2HandedWarrior");
 		GameObject go = (GameObject)Instantiate (prefab);
 		go.transform.localEulerAngles = new Vector3 (0, 180, 0);
