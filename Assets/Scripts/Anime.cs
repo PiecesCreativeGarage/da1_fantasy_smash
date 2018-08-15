74ad2e0126c237e9dcabdf2fbde95539f336c5c2
ABC <keso259w@gmail.com>
2018-08-15 18:31:05 +0900

    アニメーションスクリプト　を書きました

---
+++ Assets/Scripts/Anime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anime : MonoBehaviour {
+    private Animator animator;
+    private const string moving = "Moving";
+    private const string attack1 = "Attack1Trigger";
+
+    float x;
+    float z;
+
+	void Start () {
+        this.animator = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+        if(x != 0 || z != 0)
+        {  
+            this.animator.SetBool(moving, true);
+        }
+        else
+        {
+            this.animator.SetBool(moving, false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.animator.SetTrigger(attack1);
+        }
+    }
+}
