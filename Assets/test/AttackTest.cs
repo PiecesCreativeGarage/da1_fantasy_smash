a9dd8995efe5b0c64cd826dba09d3d68f7de4868
ABC <keso259w@gmail.com>
2018-08-16 19:22:34 +0900

    teatファイル　を作りました
    攻撃判定を作りたくて　アニメーション中に
    Swordの　コライダーの　オン/オフを　切り替えたかったのですが　
    エラーが出て　よくわからなかったので　Pushしました

---
+++ Assets/test/AttackTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTest : MonoBehaviour {
+ 
+    Collider collider;
+	void Start() {
+
+        collider = GameObject.Find("Sword").GetComponent<CapsuleCollider>();
+    
+        
+    }
+	
+	void Test() {
+
+
+        collider.enabled = true;
+    
+    }
+}
---
+++ Assets/test/AttackTestend.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTestend : MonoBehaviour
+{
+
+    GameObject h;
+
+    void Start()
+    {
+        h = GameObject.Find("Sword");
+    }
+
+    // Update is called once per frame
+    void Testend() { 
+
+            h.GetComponent<CapsuleCollider>().enabled = false;
+        
+    }
+}
