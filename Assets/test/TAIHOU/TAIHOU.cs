efa90188cb23ad140b02e6902cdcfd86eea077b9
ABC <keso259w@gmail.com>
2018-08-30 13:19:22 +0900

    Ｔａｉｈｏｕファイルをつくりました。スクリプト　と　prefab を　つくりました。
    うまくいかなかったです

---
+++ Assets/test/TAIHOU/TAIHOU.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAIHOU : MonoBehaviour {
+
+    [SerializeField]
+    GameObject TAMA;
+    [SerializeField]
+    bool Fire = false;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		if(Fire == true)
+        {
+            Instantiate(TAMA, this.transform.position, this.transform.rotation);
+            Fire = false;
+        }
+	}
+
+  
+}
---
+++ Assets/test/TAIHOU/bomb.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    public float gravity;
+    public float mass;
+    public float pow;
+
+    float f1g;
+    float Fg;
+
+
+    float Fs;
+
+
+
+    public Vector3 posi;
+
+
+    float Ac;
+
+    float Velo;
+
+    // Use this for initialization
+    private void Start()
+    {
+        f1g = gravity * mass;
+        Fg = pow;
+
+        Fs = pow;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        Fg -= f1g;
+
+        Ac = Fg / mass;
+        Velo = Ac * Time.fixedDeltaTime;
+        posi += transform.up * Velo * Time.fixedDeltaTime;
+
+        Ac = Fs / mass;
+        Velo = Ac * Time.fixedDeltaTime;
+        posi += transform.forward * Velo * Time.fixedDeltaTime;
+
+
+        transform.position = posi;
+
+    
+    }
+}
