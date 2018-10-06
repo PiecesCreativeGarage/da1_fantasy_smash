0141e2071000ceb92a020ff6a02cbaa18edb8b1d
ABC <keso259w@gmail.com>
2018-10-06 23:15:23 +0900

    メッシュのスクリプトを書こうとして失敗
    シーンaaaを消した

--- Assets/test/Game/GameOver.cs
+++ Assets/test/Game/GameOver.cs
@@ -1,34 +1,97 @@
 ﻿using UnityEngine;
+using System.Collections;
 [System.Serializable]
 public class Player_Info
 {
+    public GameObject player;
+    public bool wint_losef = true;
 
-    public Status status;
     public GameObject Camera;
+    
+    public UnityEngine.UI.Text label;
+    public string Win_message;
+    public string Lose_message;
+    
 
+    
 }
 
-public class GameOver : MonoBehaviour {
-  
+public class GameOver : MonoBehaviour
+{
+    public GameObject canvas;
+
+    //
+    public UnityEngine.UI.Text GameSet_label;
+    public string GamesetMessage;
+    public float wait_Time;
+    //
+
+    int num_of_player;
+    public bool gameset_ON;
+
     public Player_Info[] player_Info;
-    
-	void Start () {
-   
-	}
+
+    void Start()
+    {
+        num_of_player = player_Info.Length;
+    }
 
 
-    void Update() {
-        /*
-        for (int i = 0; i < player_Info.Length; i++) {
-            if (player_Info[i].status.Playing == false)
+    void Update()
+    {
+        for (int i = 0; i < player_Info.Length; i++)
+        {
+            if (player_Info[i].player == null)
             {
-                Debug.Log("aaa");
+                num_of_player--;
+                player_Info[i].wint_losef = false;
+
+            }
+            if(num_of_player <= 1)
+            {
+                gameset_ON = true;
+                break;
             }
 
-        
+
         }
-        */
-        if (player_Info[0].status == null) Debug.Log("aaa");
-        if (player_Info[1].status == null) Debug.Log("eee");
+        if(gameset_ON == true)
+        {
+            //
+            GameSet_label.enabled = true;
+            GameSet_label.text = GamesetMessage;
+            StartCoroutine("Wait");
+            
+
+            //
+            Gameset();
+            Time.timeScale = 0;
+            
+        }
+        
+    }
+    void Gameset()
+    {
+        
+
+        for (int i = 0; i < player_Info.Length; i++)
+        {
+           if(player_Info[i].wint_losef == true)
+            {
+                player_Info[i].label.text = player_Info[i].Win_message;
+            }
+            if (player_Info[i].wint_losef == false)
+            {
+                player_Info[i].label.text = player_Info[i].Lose_message;
+            }
+        }
+
+        canvas.SetActive(true);
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(wait_Time);
     }
 }
+
+
---
+++ Assets/test/MeshTest/MeshTTT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTTT : MonoBehaviour {
+    [System.Serializable]
+    public struct AAA
+    {
+        public Vector3[] vector3s;
+    }
+    public AAA[] aaa;
+    public int a;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            aaa[1].vector3s = new Vector3[aaa[0].vector3s.Length + 6];
+            for(a = 0; a < aaa[0].vector3s.Length; a++)
+            {
+                aaa[1].vector3s[a] = aaa[0].vector3s[a];
+            }
+        }
+	}
+}
